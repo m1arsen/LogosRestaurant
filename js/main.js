0ca60aead40bccd17b806265021fe259a5612c24
m1arsen <m1arsen.web@gmail.com>
2023-01-26 15:22:59 +0300

    connecting the slider

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 1,
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+  // allowTouchMove: true,
+  // autoHeight: true,
+  // grabCursor: true,
+
+});
